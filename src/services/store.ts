5333682b6882557d1e2c6aea09e7d720555e4b28
akm762xm4 <shahfarhan4451@gmail.com>
2023-12-16 16:33:35 +0530

    store changes

--- src/services/store.ts
+++ src/services/store.ts
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit"
 import { api } from "./server-api"
-import { cloudinaryApi } from "../features/photo/api/uploadApi"
 export const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
-    [cloudinaryApi.reducerPath]: cloudinaryApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(api.middleware),
